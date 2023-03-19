1525ef6b79658ede6b1630946a3c0f18d1a9175d
jpolstre <jpolstre@gmail.com>
2023-03-19 14:02:45 -0400

    update dependencies

--- example/android/app/src/main/kotlin/it/aesys/flutter_video_cast_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/it/aesys/flutter_video_cast_example/MainActivity.kt
@@ -1,4 +1,4 @@
-package it.aesys.flutter_cast_video_example
+package it.aesys.flutter_video_cast_example
 
 import com.google.android.gms.cast.framework.CastContext
 import io.flutter.embedding.android.FlutterFragmentActivity
@@ -6,8 +6,21 @@ import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterFragmentActivity() {
+    //la app principal debe ser FlutterFragmentActivity
+    //en AndroidManifet.xml agregar: android:theme="@style/Theme.AppCompat.NoActionBar"
+    //se debe sobre escribir esto tal como esta:
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         GeneratedPluginRegistrant.registerWith(flutterEngine)
-        CastContext.getSharedInstance(applicationContext)
+    //ok actualizado para 'com.google.android.gms:play-services-cast-framework:21.2.0'
+        CastContext.getSharedInstance(applicationContext,  Ejecutor())
     }
+
+}
+//ok funca!!!
+class Ejecutor: java.util.concurrent.Executor{
+    override fun execute(p0: Runnable?) {
+        p0?.run()
+
+    }
+
 }
